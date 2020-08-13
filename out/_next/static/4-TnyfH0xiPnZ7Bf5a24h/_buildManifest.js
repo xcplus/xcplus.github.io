946f6055e0b1e9582b62b1f5eaaf0b76ae4f0cc0
Lovesx <golovesx@gmail.com>
2020-08-13 12:50:06 +0800

    init

---
+++ out/_next/static/4-TnyfH0xiPnZ7Bf5a24h/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-9ed73a5fc941edae01bf.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
