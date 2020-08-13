9bbe4492419d889dd0f2db4b5990bb5b66d25da7
Lovesx <golovesx@gmail.com>
2020-08-13 13:01:03 +0800

    Deploy to master from source

---
+++ _next/static/g-VesJRC6yoL0az6YJUZZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-8895dbc9180a6d76d010.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ff3673b4e910f9435a1e.js"],"/about":["static\u002Fchunks\u002Fpages\u002Fabout-71953afe41369d163a7a.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
