4947aaf45ca26ec32d94c45309c97e7a0c3da046
Lovesx <golovesx@gmail.com>
2020-08-18 18:03:21 +0800

    pages

--- pages/about.js
+++ pages/about.js
@@ -1,65 +1,12 @@
-import React from 'react'
+import Link from 'next/link'
 
-function BlogPostPage(props) {
+const About = () => {
   return (
-    <div>
-      <h1>{props.blog.title}</h1>
-      <section dangerouslySetInnerHTML={{__html: props.blog.content}}></section>
-    </div>
+    <>
+    <h1>About</h1>
+    <Link href="/"><a>Home</a></Link>
+    </>
   )
 }
 
-// 在构建时在服务器端调用此函数。
-export async function getStaticProps(context) {
-  const fs = require('fs')
-  const html = require('remark-html')
-  const highlight = require('remark-highlight.js')
-  const unified = require('unified')
-  const markdown = require('remark-parse')
-  const matter = require('gray-matter')
-
-  const slug = context.params.slug
-  const path = `${process.cwd()}/contents/${slug}.md`;
-  const rawContent = fs.readFileSync(path, {encoding: 'utf-8'})
-  const {data, content} = matter(rawContent)
-
-  const result = await unified()
-    .use(markdown)
-    .use(highlight) // highlight code block
-    .use(html)
-    .process(content)
-
-  return {
-    props: {
-      blog: {
-        ...data,
-        content: result.toString()
-      }
-    }
-  }
-}
-
-// 在构建时生成HTML路径
-export async function getStaticPaths(context) {
-  const fs = require('fs')
-
-  const path = `${process.cwd()}/contents`
-  const files = fs.readdirSync(path, "utf-8")
-
-  const markdownFileNames = files
-    .filter((fn) => fn.endsWith(".md"))
-    .map((fn) => fn.replace(".md", ""))
-
-  return {
-    paths: markdownFileNames.map((fileName) => {
-      return {
-        params: {
-          slug: fileName
-        }
-      }
-    }),
-    fallback: false
-  }
-}
-
-export default BlogPostPage
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,82 @@
-import React from 'react'
 import Link from 'next/link'
 
-function IndexPage(props) {
+const Index = () => {
   return (
     <div>
-      <h1>Blog List</h1>
+      <div className="bg-indigo-600 px-8 py-4 text-center">
+        <Link href="/">
+          <a className="mr-5 text-gray-200">首页</a>
+        </Link>
+        <Link href="/tech">
+          <a className="mr-5 text-gray-200">技术</a>
+        </Link>
+        <Link href="/life">
+          <a className="mr-5 text-gray-200">生活</a>
+        </Link>
+        <Link href="/about">
+          <a className="mr-5 text-gray-200">关于</a>
+        </Link>
+      </div>
+      <div className="flex flex-row mx-64">
+        <div className="w-3/4">
+          <div className="py-8">
+            <h4 className="font-normal h-10 text-xl text-indigo-600">
+              我的第一篇博客
+            </h4>
+            <div>
+              <small className="h-8 text-sm font-thin">标签: ruby,elixir</small> •  
+              <small  className="h-8 text-sm font-thin">分类: 生活</small> •
+              <small  className="h-8 text-sm font-thin">2020-08-18</small>
+            </div>
+            <p className="h-10 text-lg font-light my-2">心有多大，路有多宽心有多大，路有多宽心有多大，路有多宽 心有多大，路有多宽心有多大，路有多宽心有多大，路有多宽心有多大，路有多宽心有多大，路有多宽心有多大，路有多宽 ...... <Link href="/"><a className="text-gray-600 text-opacity-75" >阅读更多</a></Link></p>
+            
+          </div>
 
-      <ul>
-        {props.blogs.map((blog, index) => {
-          return (
-            <li key={blog.id}>
-              <Link href={`/blog/${blog.slug}`}>
-                <a>{blog.title}</a>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
+          <div className="py-8">
+            <h4 className="font-normal h-10 text-xl text-indigo-600">
+              Ruby 中的继承
+            </h4>
+            <div>
+              <small className="h-8 text-sm font-thin">标签: ruby,elixir</small> •  
+              <small  className="h-8 text-sm font-thin">分类: 生活</small> •
+              <small  className="h-8 text-sm font-thin">2020-08-18</small>
+            </div>
+            <p className="h-10 text-lg font-light my-2">心有多大，路有多宽心有多大，路有多宽心有多大，路有多宽 ...... <Link href="/"><a className="text-gray-600 text-opacity-75" >阅读更多</a></Link></p>
+          </div>
+          <div className="py-8">
+            <h4 className="font-normal h-10 text-xl text-indigo-600">
+              ACE加密
+            </h4>
+            <div>
+              <small className="h-8 text-sm font-thin">标签: ruby,elixir</small> •  
+              <small  className="h-8 text-sm font-thin">分类: 生活</small> •
+              <small  className="h-8 text-sm font-thin">2020-08-18</small>
+            </div>
+            <p className="h-10 text-lg font-light my-2">心有多大，路有多宽 心有多大，路有多宽心有多大，路有多宽...... <Link href="/"><a className="text-gray-600 text-opacity-75" >阅读更多</a></Link></p>
+          </div>
+          <div className="py-8">
+            <h4 className="font-normal h-10 text-xl text-indigo-600">
+              通过 Mina 自动部署 Ruby 项目
+            </h4>
+            <div>
+              <small className="h-8 text-sm font-thin">标签: ruby,elixir</small> •  
+              <small  className="h-8 text-sm font-thin">分类: 生活</small> •
+              <small  className="h-8 text-sm font-thin">2020-08-18</small>
+            </div>
+            <p className="h-10 text-lg font-light my-2">心有多大，路有多宽心有多大，路有多宽心有多大，路有多宽心有多大，路有多宽...... <Link href="/"><a className="text-gray-600 text-opacity-75" >阅读更多</a></Link></p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-gray-400 bg-opacity-25 border-t-4 border-b-4 p-4 h-64 ml-4 my-8 w-1/4">
+          <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="https://tailwindcss.com/_next/static/media/erin-lindford.4804b52007ca82ebe9999d19c717b44d.jpg" />
+          <p>当下努力，未来可期</p>
+          <div className="flex flex-row justify-between">
+            <div>github</div>
+            <div>twitter</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-// 在构建时在服务器端调用此函数。
-export async function getStaticProps() {
-  const fs = require('fs')
-  const matter = require('gray-matter')
-  const {v4: uuid} = require('uuid')
-
-  const files = fs.readdirSync(`${process.cwd()}/contents`, 'utf-8')
-
-  const blogs = files.filter((fn) => fn.endsWith('.md'))
-    .map((fn) => {
-      const path = `${process.cwd()}/contents/${fn}`
-      const rawContent = fs.readFileSync(path, {
-        encoding: 'utf-8'
-      })
-      const { data } = matter(rawContent)
-
-      return {...data, id: uuid()}
-    })
-    return { props: { blogs } }
-}
-
-export default IndexPage;
+export default Index;
